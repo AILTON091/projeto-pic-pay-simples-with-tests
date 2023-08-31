7a53c35b32f09e0018894b7d1ce7dd0bba1abd0d
Ailton da Costa <ailtondacosta-91@hotmail.com>
2023-08-31 15:03:27 -0300

    feat: create Transation entity

---
+++ src/main/java/com/picpaysimplificado/picpaysimplificado/domain/transaction/Transaction.java
@@ -0,0 +1,27 @@
+package com.picpaysimplificado.picpaysimplificado.domain.transaction;
+
+import com.picpaysimplificado.picpaysimplificado.domain.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity(name = "transactions")
+@Table(name = "transactions")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal amount;
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+
+}
